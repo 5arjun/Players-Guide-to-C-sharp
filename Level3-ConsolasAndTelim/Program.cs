bd6ac3e51cb9242c18d158d61fbe48b2e97095dc
Arjun Patel <tuo45748@temple.edu>
2025-05-28 21:31:04 -0400

    level 3

---
+++ Level3-ConsolasAndTelim/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bread is ready!");
+Console.WriteLine("Who wants some bread?");
+string name = Console.ReadLine();
+Console.WriteLine("Noted: " + name + " got some bread!");
