2e76f2f4ee22c96431cc1815c8215593c36a0134
Arjun Patel <tuo45748@temple.edu>
2025-05-28 21:40:18 -0400

    level 4

---
+++ Level4-TheThingNamer3000/Program.cs
@@ -0,0 +1,14 @@
+﻿//asks for a thing and a description, then prints it out in a specific format
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine();
+//string a is the thing, like "sword", "shield", etc.
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine();
+//string b is the description, like "big", "azure", "tattered", etc.
+string c = "of Doom";
+string d = "3000";
+/*
+string c is the suffix, like "of Doom", "of Destiny", etc.
+string d is the number, like "3000", "9000", etc.
+*/
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
