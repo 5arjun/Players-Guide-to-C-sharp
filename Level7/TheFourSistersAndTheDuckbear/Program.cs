fa664db3f5009648f0cbf35d7f68cb3ae319d3e3
Arjun Patel <tuo45748@temple.edu>
2025-06-02 22:57:36 -0400

    7- sisters and the duckbear

---
+++ Level7/TheFourSistersAndTheDuckbear/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("How many apples were collected?");
+int apples = Convert.ToInt32(Console.ReadLine());
+
+int leftover = apples % 4;
+Console.WriteLine("The number of leftover apples is: " + leftover);
+Console.WriteLine("The number of apples that can be evenly distributed to the sisters is: " + (apples - leftover) / 4);
