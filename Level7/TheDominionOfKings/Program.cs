627d5a58a2d0e22cae7b8ed53b02238a783842b3
Arjun Patel <tuo45748@temple.edu>
2025-06-03 18:49:27 -0400

    dominion of kings 7

---
+++ Level7/TheDominionOfKings/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter provinces");
+int provinces = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter Duchies");
+int duchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter estates");
+int estates = Convert.ToInt32(Console.ReadLine());
+
+int point = provinces * 6 + duchies * 3 + estates ;
+Console.WriteLine("The num of points you have is " + point);
