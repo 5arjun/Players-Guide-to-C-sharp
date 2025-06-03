180dd1ffd2b98cd6cdaf51834ca5307c94280b58
Arjun Patel <tuo45748@temple.edu>
2025-06-02 22:49:15 -0400

    level 7 the triangle farmer

---
+++ Level7/TheTriangleFarmer/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter base of the triangle:");
+int Base = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter height of the triangle:");
+int height = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("The area of the triangle is: " + (0.5 * Base * height));
