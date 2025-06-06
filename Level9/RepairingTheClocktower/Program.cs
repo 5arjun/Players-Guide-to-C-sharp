d40c20ec3ba4a8fca5e279671f05e5eabc3efddb
Arjun Patel <tuo45748@temple.edu>
2025-06-06 11:27:49 -0400

    9- defense of clocktower

---
+++ Level9/RepairingTheClocktower/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+    Console.WriteLine("Your nums even");
+else
+{
+    Console.WriteLine("Your nums odd");
+}
