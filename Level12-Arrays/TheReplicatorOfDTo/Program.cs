7e82492542a0744c93b8c29d1f9cfe9cf3a557eb
Arjun Patel <tuo45748@temple.edu>
2025-06-22 09:57:15 -0400

    12- the replicator

---
+++ Level12-Arrays/TheReplicatorOfDTo/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] user = new int[5];
+for (int i = 0; i < user.Length; i++)
+{
+    Console.WriteLine($"Enter value for spot number {i}");
+    user[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] copy = new int[5];
+for (int i = 0; i < user.Length; i++)
+{
+    copy[i] = user[i];
+}
+
+for (int i = 0; i < user.Length; i++)
+{
+    Console.WriteLine($"user[{i}] = {user[i]}");
+    Console.WriteLine($"copy[{i}] = {user[i]}");
+}
