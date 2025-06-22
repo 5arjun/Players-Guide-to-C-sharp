ff86a1ba0e0366f1b127fae96d5af5c54fb2e361
Arjun Patel <tuo45748@temple.edu>
2025-06-22 10:01:20 -0400

    12- laws of freach

--- Level12-Arrays/TheLawsOfFreach/Program.cs
+++ Level12-Arrays/TheLawsOfFreach/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+foreach(int num in array)
+{
+    if (num < currentSmallest)
+        currentSmallest = num;
+}
+Console.WriteLine(currentSmallest);
+
+int[] array2 = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int total = 0;
+foreach(int num in array2)
+    total += num;
+float average = (float)total / array.Length;
+Console.WriteLine(average);
