0dcc5f268f5ee3aa0a2ea6864d70a3064a864820
Arjun Patel <tuo45748@temple.edu>
2025-06-24 12:24:15 -0400

    13- taking a num

---
+++ Level13-Methods/TakingANumber/Program.cs
@@ -0,0 +1,22 @@
+﻿int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int AskForNumberInRange(string text, int min, int max)
+{
+    int num = Int32.MinValue;
+    while (true)
+    {
+        num = AskForNumber(text);
+        if (num < min || num > max)
+            num = AskForNumberInRange(text, min, max);
+        return num;
+    }
+}
+
+int number = AskForNumber("Enter a number broski");
+Console.WriteLine($"you entered: {number}");
+
+int num2 = AskForNumberInRange("Enter a num betweek 2 and 5 broski", 2, 5);
+Console.WriteLine($"you entered: {num2}");
