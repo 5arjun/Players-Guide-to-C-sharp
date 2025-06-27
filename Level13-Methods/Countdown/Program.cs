f500a041ce3d4a855a527ff65e1ced16b5670160
Arjun Patel <tuo45748@temple.edu>
2025-06-26 22:00:33 -0400

    13- countdown

---
+++ Level13-Methods/Countdown/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+void Countdown(int x)
+{
+    Console.WriteLine(x);
+    if (x == 0) return;
+    Countdown(--x);
+}
+
+Countdown(10);
