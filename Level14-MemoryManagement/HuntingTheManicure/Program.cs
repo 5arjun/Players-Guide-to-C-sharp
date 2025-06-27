7e2af69cbdfab872ebfdbcaedbebf20a5df4fbaa
Arjun Patel <tuo45748@temple.edu>
2025-06-27 15:57:07 -0400

    14- manticure

--- Level14-MemoryManagement/HuntingTheManicure/Program.cs
+++ Level14-MemoryManagement/HuntingTheManicure/Program.cs
@@ -1,2 +1,96 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Player 2, it is your turn.
+// -----------------------------------------------------------
+// STATUS: Round: 1 City: 15/15 Manticore: 10/10
+// The cannon is expected to deal 1 damage this round.
+// Enter desired cannon range: 50
+// That round OVERSHOT the target.
+
+void DrawLine()
+{
+    Console.WriteLine("--------------------------------------------------");
+}
+
+string status(int round, int city, int manticore)
+{
+    return $"STATUS: Round: {round} City: {city}/15 Manticore: {manticore}/10\n" +
+           $"The cannon is expected to deal {cannonDamage(round)} damage this round.";
+}
+int cannonDamage (int round)
+{
+    if (round % 3 == 0 && round % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        return 5;
+    }
+    else if (round % 3 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        return 3;
+    }
+    else if (round % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        return 3;
+    }
+    else
+    {
+        Console.ResetColor();
+        return 1;
+    }
+}
+
+bool rangeCheck(int range, int target)
+{
+    if (range < 1 || range > 100)
+    {
+        Console.WriteLine("Range must be between 1 and 100. MISS");
+        return false;
+    }
+    if (range < target)
+    {
+        Console.WriteLine("That round UNDERSHOT the target.");
+        return false;
+    }
+    if (range > target)
+    {
+        Console.WriteLine("That round OVERSHOT the target.");
+        return false;
+    }
+
+    if (range == target)
+    {
+        Console.WriteLine("That round HIT the target!");
+        return true;
+    }
+    return true;
+}
+
+
+int round = 1;
+int city = 15;
+int manticore = 10;
+
+Console.Write("Player 1, how far away from the city do you want to station the Manticore? ");
+int target = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Player 2, it is your turn.");
+do
+{
+    DrawLine();
+    Console.WriteLine(status(round, city, manticore));
+    Console.Write("Enter desired cannon range: ");
+    int range = Convert.ToInt32(Console.ReadLine());
+    if (rangeCheck(range, target))
+    {
+        manticore -= cannonDamage(round);
+        if (manticore <= 0)
+        {
+            Console.WriteLine("You have defeated the Manticore!");
+        }
+    }
+    city--;
+    if (city <= 0)
+    {
+        Console.WriteLine("The city has been destroyed!");
+    }
+    round++;
+} while (manticore > 0 && city > 0);
