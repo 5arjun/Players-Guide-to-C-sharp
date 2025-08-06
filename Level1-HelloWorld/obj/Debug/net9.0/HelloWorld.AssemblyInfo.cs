4e14952fa0fbd0904f362366d2c75548a0375530
Arjun Patel <tuo45748@temple.edu>
2025-08-06 14:13:41 -0400

    pr

--- Level1-HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ Level1-HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee6d0f4dd83658c73a209831ae4e712d6d38b22a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73af243e60ca210088f18cf433a660882d9421e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
