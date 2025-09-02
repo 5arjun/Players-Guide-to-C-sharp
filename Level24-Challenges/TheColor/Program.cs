86340c5b204d21d83112d66b9db4ed003ea67abb
Arjun Patel <arjunpat107@gmail.com>
2025-09-01 20:24:02 -0400

    finished

--- Level24-Challenges/TheColor/Program.cs
+++ Level24-Challenges/TheColor/Program.cs
@@ -18,10 +18,19 @@ class Color
     public static Color makeRed() => new Color(255, 0, 0);
     public static Color makeGreen() => new Color(0, 128, 0);
     public static Color makeBlue() => new Color(0, 0, 255);
-    public static Color makeOrange() => new Color(255,165,0);
-    public static Color makeYellow() => new Color(255,255,0);
-    public static Color makePurple() => new Color(128,0,128);
+    public static Color makeOrange() => new Color(255, 165, 0);
+    public static Color makeYellow() => new Color(255, 255, 0);
+    public static Color makePurple() => new Color(128, 0, 128);
+}
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Color myColor = new Color(30, 150, 155);
+        Color purple = Color.makePurple();
 
-
+        Console.WriteLine($"myColor values:\n  red-{myColor.red}\n  green-{myColor.green}\n  blue-{myColor.blue}");
+        Console.WriteLine($"Purple values:\n  red-{purple.red}\n  green-{purple.green}\n  blue-{purple.blue}");
+    }
 }
