c3a281513ce9693aafd932b9e68e616ddd2b800f
Arjun Patel <arjunpat107@gmail.com>
2025-09-01 20:17:14 -0400

    static constructors

--- Level24-Challenges/TheColor/Program.cs
+++ Level24-Challenges/TheColor/Program.cs
@@ -6,4 +6,22 @@ class Color
     public int green { get; set; }
     public int blue { get; set; }
 
+    public Color(int r, int g, int b)
+    {
+        red = r;
+        green = g;
+        blue = b;
+    }
+
+    public static Color makeWhite() => new Color(255, 255, 255);
+    public static Color makeBlack() => new Color(0, 0, 0);
+    public static Color makeRed() => new Color(255, 0, 0);
+    public static Color makeGreen() => new Color(0, 128, 0);
+    public static Color makeBlue() => new Color(0, 0, 255);
+    public static Color makeOrange() => new Color(255,165,0);
+    public static Color makeYellow() => new Color(255,255,0);
+    public static Color makePurple() => new Color(128,0,128);
+
+
+
 }
