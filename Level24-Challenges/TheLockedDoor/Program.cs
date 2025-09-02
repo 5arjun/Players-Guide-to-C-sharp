35a673a6a42fbde2f2be966c25759cca4af2345b
Arjun Patel <arjunpat107@gmail.com>
2025-09-01 21:36:54 -0400

    started the class

---
+++ Level24-Challenges/TheLockedDoor/Program.cs
@@ -0,0 +1,17 @@
+﻿public enum State { open, closed, locked };
+
+public class Door
+{
+    public State state { get; set; } = State.open;
+    private string code { get; set; }
+
+    public Door(string initialCode) { code = initialCode; }
+
+    public void closeDoor() { if (state == State.open) state == State.closed; }
+    public void openDoor() { if (state == State.closed) state == State.open; }
+
+    public void lockDoor() { if (state == State.locked) state == State.Locked; }
+
+    public void unlockDoor(string code) { if (this.code == code && state == State.locked) state == State.closed; }
+
+}
