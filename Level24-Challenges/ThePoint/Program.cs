78400f195dc110daf045667afbaadab542796479
Arjun Patel <arjunpat107@gmail.com>
2025-09-01 20:02:02 -0400

    done the Point

--- Level24-Challenges/ThePoint/Program.cs
+++ Level24-Challenges/ThePoint/Program.cs
@@ -1,4 +1,6 @@
-﻿class Point
+﻿using System;
+
+class Point
 {
     public int x { get; set; }
     public int y { get; set; }
@@ -14,3 +16,18 @@
         y = 0;
     }
 }
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point myPoint = new Point(2, 3);
+        Point myPoint2 = new Point(-4, 0);
+
+        Console.WriteLine($"Point 1:({myPoint.x},{myPoint.y})");
+        Console.WriteLine($"Point 2:({myPoint2.x},{myPoint2.y})");
+
+    }
+}
+
+//Answer this question: Are your X and Y properties immutable? Why did you choose what you did?
+//they're mutable bc what if i want to change the coordinates of an object
