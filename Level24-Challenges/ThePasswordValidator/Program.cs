0d865b8a87513df856eb6632f83baf3f52e797d3
Arjun Patel <arjunpat107@gmail.com>
2025-09-02 21:40:29 -0400

    finished validator

--- Level24-Challenges/ThePasswordValidator/Program.cs
+++ Level24-Challenges/ThePasswordValidator/Program.cs
@@ -1,29 +1,27 @@
 ﻿using System;
 
-public class PasswordValidator
+public static class PasswordValidator
 {
-    public string password { get; set; }
-    private bool isValid { get; set; }
-
-    public bool checkLength(string password)
+ 
+    public static bool checkLength(string password)
     {
         if (password.Length >= 6 && password.Length <= 13)
             return true;
         return false;
     }
 
-    public bool checkCaseAndNum(string password)
+    public static bool checkCaseAndNum(string password)
     {
         int[] count = { 0, 0, 0 };
         foreach (char letter in password)
         {
-            if (count[0] == 0 && char.isLower(letter)) count[0]++;
-            if (count[1] == 0 && char.isUpper(letter)) count[1]++;
-            if (count[2] == 0 && char.isDigit(letter)) count[2]++;
+            if (count[0] == 0 && char.IsLower(letter)) count[0]++;
+            if (count[1] == 0 && char.IsUpper(letter)) count[1]++;
+            if (count[2] == 0 && char.IsDigit(letter)) count[2]++;
         }//this was done in a stupid way w an arry. i shouldve done it differently.
         return Array.TrueForAll(count, element => element == 1);
     }
-    public bool checkDumbRules(string password)
+    public static bool checkDumbRules(string password)
     {
         bool hasT = false;
         bool hasAnd = false;
@@ -36,8 +34,20 @@ public class PasswordValidator
         return true;
     }
 
-    public bool validatePassword(string password)
+    public static bool validatePassword(string password)
     {
         return checkLength(password) && checkCaseAndNum(password) && checkDumbRules(password);
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Enter a password to check: ");
+            string password = Console.ReadLine();
+            Console.WriteLine($"Is this password valid?  {PasswordValidator.validatePassword(password)}");
+        }
     }
 }
